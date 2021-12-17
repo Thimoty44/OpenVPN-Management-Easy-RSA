9a83532eff9bc4508bc21999033a4166c16fbcef
Thimoty44 <thimotygaming@protonmail.com>
2021-12-17 13:40:15 +0100

    Update ip.php

--- ip.php
+++ ip.php
@@ -20,18 +20,24 @@
 								$ipp = fopen( $ippfile, "r" );
 								/* Loop who read every line of the ipp.txt file */
 								while ($ippdata = fgets($ipp, 1000) ) {
-								/* Regex with the pattern to find name of certificate */
-								$numberspattern = '*,$';
-								/* Regex with the pattern who find the ip */
-								$numberspattern2 = '/([0-9]+\.[0-9]+\.[0-9]+\.)([0-9]+)/';
-								/* Function who find the occurence of the ip */
-								preg_match($numberspattern2, $ippdata, $output_ip);
-								/* Calcul of the ip (not the good ip because openvpn use 3 ip for one) */
-								$numbersipfinal ="".(int)$output_ip[2].""+2;
-								/* Function who find the occurence of the certificate name */
-								preg_match("/(.*),/", $ippdata, $output_split);
-								/* We shot the waiting result for show ip */
-								echo "<p>".$output_split[0]." ".$output_ip[1]."".$numbersipfinal."</p>";
+									/* Regex with the pattern to find name of certificate */
+										$numberspattern = '*,$';
+										/* Regex with the pattern who find the ip */
+										$numberspattern2 = '/([0-9]+\.[0-9]+\.[0-9]+\.)([0-9]+)/';
+										/* Function who find the occurence of the ip */
+										preg_match($numberspattern2, $ippdata, $output_ip);
+										/* Calcul of the ip (not the good ip because openvpn use 3 ip for one) */
+										$numbersipfinal ="".(int)$output_ip[2].""+2;
+										/* Function who find the occurence of the certificate name */
+										preg_match("/(.*),(.*)/", $ippdata, $output_split);
+									/* Condition who check if ipv6 used (show raw ip if ipv6 */
+									if ($ipv6 == "false") {
+										/* We shot the waiting result for show ipv4 */
+										echo "<p>".$output_split[1]." ".$output_ip[1]."".$numbersipfinal."</p>";
+									} else {
+										/* Ip in ipv6 */
+										echo "<p>".$output_split[1]." ".$output_split[2]."</p>";
+									}
 								}
 							?>
 							<br>
